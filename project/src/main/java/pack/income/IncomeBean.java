3961ebb9e0d6c87b4c2a480ec4fe8a309cb0e8fd
minhyeong <83685136+minhyeong0208@users.noreply.github.com>
2024-07-03 16:23:05 +0900

    Delete project directory

--- project/src/main/java/pack/income/IncomeBean.java
+++
@@ -1,9 +0,0 @@
-package pack.income;
-
-import lombok.Data;
-
-@Data
-public class IncomeBean {
-	private String teamcode, briefs, date;
-	private int code, ticket, goods, fan, broad, spon, ad, rent, loan, pysell;
-}
